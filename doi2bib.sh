8903c3e863f50fce9c5e1998d05c1fb542628bad
Lugges991 <29121982+Lugges991@users.noreply.github.com>
2021-05-09 15:52:48 +0200

    cleaning of generated bibtex by removing unnecessary %

--- doi2bib.sh
+++ doi2bib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl "https://doi2bib.org/2/doi2bib?id=$(xclip -o | sed 's/https:\/\///' | sed 's/doi.org\///')" | xclip
+curl "https://doi2bib.org/2/doi2bib?id=$(xclip -o | sed 's/https:\/\///' | sed 's/doi.org\///')" | sed 's/%//' | xclip
